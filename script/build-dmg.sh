bd7dd83d64a3fd341e298e3c10a403b94c120e77
Sergei Garin <sergei@garin.dev>
2026-06-02 15:36:46 +0300

    Stage app bundle before creating DMG

--- script/build-dmg.sh
+++ script/build-dmg.sh
@@ -6,18 +6,23 @@ VERSION="${1:-${RAYXDR_VERSION:-0.1.0}}"
 BUILD_NUMBER="${RAYXDR_BUILD_NUMBER:-${GITHUB_RUN_NUMBER:-1}}"
 DIST_DIR="$ROOT/dist"
 DMG_PATH="$DIST_DIR/RayXDR-$VERSION.dmg"
+STAGE_DIR="$DIST_DIR/dmg-root"
 
 rm -rf "$DIST_DIR"
-mkdir -p "$DIST_DIR"
+mkdir -p "$STAGE_DIR"
 
 APP_DIR="$(RAYXDR_VERSION="$VERSION" RAYXDR_BUILD_NUMBER="$BUILD_NUMBER" "$ROOT/script/build-menubar-app.sh" | tail -n 1)"
+STAGED_APP_DIR="$STAGE_DIR/RayXDR.app"
 
-/usr/bin/codesign --force --deep --sign - "$APP_DIR"
+/usr/bin/ditto "$APP_DIR" "$STAGED_APP_DIR"
+/usr/bin/codesign --force --deep --sign - "$STAGED_APP_DIR"
 /usr/bin/hdiutil create \
   -volname "RayXDR" \
-  -srcfolder "$APP_DIR" \
+  -srcfolder "$STAGE_DIR" \
   -ov \
   -format UDZO \
   "$DMG_PATH"
+
+rm -rf "$STAGE_DIR"
 
 echo "$DMG_PATH"
